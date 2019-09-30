f6551d6ff51c780aec83b42f2929977555f918df
Vinod Sonagara <vksonagara1996@gmail.com>
2019-10-01 00:14:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: []
+  };
+  render() {
+    return (
+      <div className="App">
+        <TodoForm triggerAddTodo={this.addTodo}/>
+        <TodoList todos={this.state.todos}/>
+      </div>
+    );
+  }
+
+  addTodo = (todo) => {
+    const todos = this.state.todos;
+    todos.push(todo);
+    this.setState({
+      ...this.state,
+      todos
+    });
+  };
 }
 
 export default App;
